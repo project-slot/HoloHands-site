00007ba92fe0d3cf64ac0aa0fea1b5dd19f8a1c5
TungVLe <tunle0801@gmail.com>
2023-12-02 17:51:15 -0500

    feat: images and videos update

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var width = $(window).width()
 window.onscroll = function () {
 	if (width >= 1000) {
 		if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-			$('#header').css('background', 'rgba(0,0,0,0.7)')
-			$('#header').css('color', 'var(--secondary)')
-			$('#header').css('box-shadow', '0px 0px 20px rgba(0,0,0,0.09)')
+			$('#header').css('background', 'rgba(0,0,0,0)')
+			$('#header').css('color', 'var(--text-primary)')
 			$('#header').css('padding', '4vh 4vw')
 			$('#navigation a').hover(
 				function () {
@@ -15,10 +14,9 @@ window.onscroll = function () {
 				}
 			)
 		} else {
-			$('#header').css('background', 'transparent')
-			$('#header').css('color', '#fff')
-			$('#header').css('box-shadow', '0px 0px 0px rgba(0,0,0,0)')
-			$('#header').css('padding', '6vh 4vw')
+			$('#header').css('background', 'rgba(0, 0, 0, 0)')
+			$('#header').css('color', 'var(--text-tertiary)')
+			$('#header').css('padding', '4vh 4vw')
 			$('#navigation a').hover(
 				function () {
 					$(this).css('border-bottom', '2px solid #fff')
